30f1ea68debd9796f8ba432dc288f874caf53c01
VitorHScholze <hugovitor565@gmail.com>
2026-05-05 11:23:49 -0300

    app.py

--- Etiqueta_app/app.py
+++ Etiqueta_app/app.py
@@ -5,36 +5,34 @@ from reportlab.lib.units import cm
 from reportlab.pdfgen import canvas
 from reportlab.lib.utils import ImageReader
 from io import BytesIO
-
-st.title("Etiquetas 8x2 cm com QR Code")
-
+ 
+st.title("Etiquetas 65x25 mm com QR Code")
+ 
 file = st.file_uploader("Envie o Excel", type=["xlsx"])
-
+ 
 if file:
     df = pd.read_excel(file)
-
     st.dataframe(df)
-
+ 
     if st.button("Gerar PDF"):
-
+ 
         buffer = BytesIO()
-
-        largura = 8 * cm
-        altura = 2 * cm
-
+ 
+        largura = 6.5 * cm
+        altura = 2.5 * cm
+ 
         c = canvas.Canvas(buffer, pagesize=(largura, altura))
-
+ 
         for i, row in df.iterrows():
             texto = str(row[0])
-
-            # QR Code
+ 
             qr = qrcode.make(texto)
             qr_io = BytesIO()
             qr.save(qr_io)
             qr_io.seek(0)
-
-            # Desenhar QR
+ 
             qr_size = altura - 0.2*cm
+ 
             c.drawImage(
                 ImageReader(qr_io),
                 0.1*cm,
@@ -42,16 +40,15 @@ if file:
                 qr_size,
                 qr_size
             )
-
-            # TEXTO EM NEGRITO
+ 
             c.setFont("Helvetica-Bold", 10)
             c.drawString(qr_size + 0.3*cm, altura/2, texto[:40])
-
+ 
             c.showPage()
-
+ 
         c.save()
         buffer.seek(0)
-
+ 
         st.download_button(
             "Baixar PDF",
             buffer,
